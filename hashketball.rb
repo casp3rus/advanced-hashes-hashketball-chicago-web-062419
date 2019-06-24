948d018da6c5f51813a43c99f206e53438f8facd
Adrian Popan <adrian.popan@gmail.com>
2019-06-24 02:31:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,12 +155,15 @@ end
 
 def player_numbers(team_name)
   nums = []
-  game_hash.each do |place, team|
+  game_hash.each do |_place, team|
     next unless team[:team_name] == team_name
+
     team.each do |attribute, data|
       next unless attribute == :players
-      nums << data[:number]
-    end
+
+      data.each do |data|
+        nums << data[:number]
+      end
     end
   end
   nums
